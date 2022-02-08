1db714756911319e7c480c86672cc002426a8b17
TecnmITI3IEEE <98986205+TecnmITI3IEEE@users.noreply.github.com>
2022-02-08 05:35:19 -0600

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
 
+const menu = document.querySelector('.menu-container');
+const nav = document.querySelector('nav');
+
+menu?.addEventListener('click', () => {
+    if( !nav?.classList.contains('show')){
+        nav?.classList.add('show');
+    }else{
+        nav?.classList.remove('show')
+    }
+});
